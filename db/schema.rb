8cdae6e1b85534d0900384c5802c279d8a8fe114
Jon Rojnik <jon.rojnik@scv.si>
2021-03-01 18:08:35 +0100

    user post povezava

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_09_160626) do
+ActiveRecord::Schema.define(version: 2021_03_01_170205) do
 
   create_table "news", force: :cascade do |t|
     t.string "title"
@@ -19,11 +19,19 @@ ActiveRecord::Schema.define(version: 2021_02_09_160626) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
+# Could not dump table "posts" because of following StandardError
+#   Unknown type 'reference' for column 'user_id'
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
