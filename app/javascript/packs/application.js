2c56a466d9b1a19adfeb76a42578373deede72c5
Jon Rojnik <jon.rojnik@scv.si>
2021-03-07 22:29:40 +0100

    Dodan bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// CSS
+import 'scss/site'
+// JS
+import('js/site')
+
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
