f475e4544757dc7fc7997d770a3969137ea27d32
Jon Rojnik <jon.rojnik@scv.si>
2021-03-08 18:17:27 +0100

    Helperji in varnost

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_navbar_options
+    html = link_to "Domov" , root_path
+    html += link_to "Prijava" , new_user_session_path unless user_signed_in?
+    html += link_to "Registracija" , new_user_registration_path unless user_signed_in?
+    html += link_to "Odjava" , destroy_user_session_path, method: :delete if user_signed_in?
+    html
+
+  end
 end
--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,20 @@
 module NewsHelper
+  def display_news_links(news)
+    html = "<td>#{link_to 'Show', news}</td>"
+    if current_user == news.user
+      html += "<td>#{link_to 'Edit', edit_news_path(news)}</td>"
+      html += "<td>#{link_to 'Destroy', news, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
+    end
+    html.html_safe
+  end
+
+  def display_news_show_links(news)
+    html = ""
+    if current_user == news.user
+      html += link_to 'Edit', edit_news_path(news)
+      html += ' | '
+    end
+    html += link_to 'Back', news_index_path
+    html.html_safe
+  end
 end
