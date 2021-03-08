f514a9a166791917206243c40dd7b5194d33eee3
Jon Rojnik <jon.rojnik@scv.si>
2021-03-08 22:24:16 +0100

    Show pages bootstrap

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -8,12 +8,12 @@ module NewsHelper
   def display_news_show_links(news)
     html = ""
     if current_user == news.user
-      html += link_to 'Uredi', edit_news_path(news)
-      html += ' | '
-      html += link_to 'Izbriši', news, method: :delete, data: { confirm: 'Are you sure?' }
-      html += ' | '
+      html += link_to 'Uredi', edit_news_path(news) , class: "btn btn-lg btn-primary"
+      html += " ";
+      html += link_to 'Izbriši', news, method: :delete, data: { confirm: 'Are you sure?' } , class: "btn btn-lg btn-primary"
+      html += " ";
     end
-    html += link_to 'Nazaj', news_index_path
+    html += link_to 'Nazaj', news_index_path, class: "btn btn-lg btn-primary"
     html.html_safe
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,12 +8,12 @@ module PostsHelper
   def display_posts_show_links(post)
     html = ""
     if current_user == post.user
-      html += link_to 'Uredi', edit_post_path(post)
-      html += ' | '
-      html += link_to 'Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }
-      html += ' | '
+      html += link_to 'Uredi', edit_post_path(post), class: "btn btn-lg btn-primary"
+      html += ' '
+      html += link_to 'Izbriši', post, method: :delete, data: { confirm: 'Are you sure?' }, class: "btn btn-lg btn-primary"
+      html += ' '
     end
-    html += link_to 'Nazaj', posts_path
+    html += link_to 'Nazaj', posts_path, class: "btn btn-lg btn-primary"
     html.html_safe
   end
 end
