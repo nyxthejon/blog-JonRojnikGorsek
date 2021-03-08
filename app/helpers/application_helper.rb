d0296ae77bb3bf328b56a87f063caf8070e60208
Jon Rojnik <jon.rojnik@scv.si>
2021-03-08 19:54:13 +0100

    Bootstrap - news stran

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def display_navbar_options
 
-      html = link_to "Domov" , root_path
+      html = ""
       if user_signed_in?
-        html += link_to "Uredi Profil", edit_user_registration_path
-        html += link_to "Odjava" , destroy_user_session_path, method: :delete if user_signed_in?
+        html += link_to "Uredi Profil", edit_user_registration_path, class: 'btn btn-sm btn-outline-secondary'
+        html += link_to "Odjava" , destroy_user_session_path, method: :delete, class: 'btn btn-sm btn-outline-secondary'
       else
-        html += link_to "Prijava" , new_user_session_path unless user_signed_in?
-        html += link_to "Registracija" , new_user_registration_path unless user_signed_in?
+        html += link_to "Prijava" , new_user_session_path, class: 'btn btn-sm btn-outline-secondary'
+        html += link_to "Registracija" , new_user_registration_path, class: 'btn btn-sm btn-outline-secondary'
       end
 
 
--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,17 +1,16 @@
 module NewsHelper
   def display_news_links(news)
-    html = "<td>#{link_to 'Show', news}</td>"
-    if current_user == news.user
-      html += "<td>#{link_to 'Edit', edit_news_path(news)}</td>"
-      html += "<td>#{link_to 'Destroy', news, method: :delete, data: { confirm: 'Are you sure?' }}</td>"
-    end
+    html = link_to 'Preberi več', news , class: 'stretched link'
+
     html.html_safe
   end
 
   def display_news_show_links(news)
     html = ""
     if current_user == news.user
-      html += link_to 'Edit', edit_news_path(news)
+      html += link_to 'Uredi', edit_news_path(news)
+      html += ' | '
+      html += link_to 'Izbriši', news, method: :delete, data: { confirm: 'Are you sure?' }
       html += ' | '
     end
     html += link_to 'Back', news_index_path
