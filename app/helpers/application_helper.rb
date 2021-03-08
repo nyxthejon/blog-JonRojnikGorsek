4e2c2eb0db031adf10251d2cfa8db51fd170c7f9
Jon Rojnik <jon.rojnik@scv.si>
2021-03-08 18:50:32 +0100

    Prikaz imena in urejanje profila

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,16 @@
 module ApplicationHelper
   def display_navbar_options
-    html = link_to "Domov" , root_path
-    html += link_to "Prijava" , new_user_session_path unless user_signed_in?
-    html += link_to "Registracija" , new_user_registration_path unless user_signed_in?
-    html += link_to "Odjava" , destroy_user_session_path, method: :delete if user_signed_in?
-    html
 
-  end
+      html = link_to "Domov" , root_path
+      if user_signed_in?
+        html += link_to "Uredi Profil", edit_user_registration_path
+        html += link_to "Odjava" , destroy_user_session_path, method: :delete if user_signed_in?
+      else
+        html += link_to "Prijava" , new_user_session_path unless user_signed_in?
+        html += link_to "Registracija" , new_user_registration_path unless user_signed_in?
+      end
+
+
+      html.html_safe
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_171458) do
+ActiveRecord::Schema.define(version: 2021_03_08_172330) do
 
 # Could not dump table "news" because of following StandardError
 #   Unknown type 'reference' for column 'user_id'
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_03_01_171458) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
